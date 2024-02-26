4a5a41bb7c8640d43f4b1559aeef7d966e495842
Brian <32653859+BrianShoubert@users.noreply.github.com>
2024-02-26 22:28:14 +0200

    Update index.php

--- index.php
+++ index.php
@@ -21,7 +21,7 @@
 
 <h2>Главная страница</h2>
 
-</?php include 'inc/show_stats.php';?>
+</?php include 'inc/show-stats.php';?>
 
 <img src="img/picture.png" width="480" height="480" alt="PICTURE" align="right" hspace="10" vspace="10">
 </body>
