fe1e959ba1c1d2debc04d2acaa4376f0b21c96a2
Brian <32653859+BrianShoubert@users.noreply.github.com>
2024-02-26 23:26:36 +0200

    Update db.php

--- inc/db.php
+++ inc/db.php
@@ -1,1 +1,12 @@
+<?php
+// Хост {обычно localhost}
+$db_host = "localhost";
+// Имя базы данных
+$db_name = "stats";
+// Логин для подключения к базе данных
+$db_user = "root";
+// Пароль для подключения к базе данных
+$db_pass = "";
 
+$db = mysqli_connect ($db_host, $db_user, "", $db_name) or die ("Невозможно подключится к БД");
+?> </?php>
