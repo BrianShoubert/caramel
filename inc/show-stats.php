fe6c5c3e3d272b9ff523d569f877300dbc6d104a
Brian <32653859+BrianShoubert@users.noreply.github.com>
2024-02-27 19:56:49 +0200

    Update show-stats.php

--- inc/show-stats.php
+++ inc/show-stats.php
@@ -1,1 +1,13 @@
+<?php
 
+// Указываем кодировку, в которой будет получена информация из базы
+@mysqli_query ($db, 'set character_set_resylts = "utf8"');
+
+// Извлекаем статистику по текущей дате (переменная date попадает сюда из файла count.php, который, в свою
+// очередь, подключается в каждом из 4 обычных файлов)
+$res = mysqli_query($db, "SELECT `views`, `hosts` FROM `visits` WHERE `date`='$date'");
+$row = mysqli_fetch_assoc($res);
+
+echo '<p>Уникальных пользователей: ' . $row['hosts'] . '<br />';
+echo 'Просмотров: ' . $row['views'] . '</p>';
+?> </?php>
